e0a956ad7e4b7c15688af56fed9bf04016509600
Mateusz Nowak <novak.mateusz94@gmail.com>
2021-06-06 21:16:16 +0200

    Adding first views

--- course/challenges/views.py
+++ course/challenges/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def january(request):
+    return HttpResponse("Hello January!")
+
+
+def february(request):
+    return HttpResponse("Hello February")
